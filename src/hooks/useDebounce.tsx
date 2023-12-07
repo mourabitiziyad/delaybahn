4bffa2c6a842b5f172d90f0298ea3ef77b667541
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-12-07 18:08:19 +0100

    Fix default parameter value in useDebounce function

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function useDebounce<T>(value: T, delay: number = 300): T {
+export default function useDebounce<T>(value: T, delay = 300): T {
   const [debounceValue, setDebounceValue] = useState<T>(value);
 
   useEffect(() => {
