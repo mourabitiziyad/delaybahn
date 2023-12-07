1a615c05ca4fce40de45b20c654e7915d851c4f4
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-12-07 18:05:07 +0100

    ♻️ refactor: trip selection component

--- src/components/trip-selection.tsx
+++ src/components/trip-selection.tsx
@@ -1,14 +1,31 @@
-import { CardTitle, CardDescription, CardHeader, CardContent, CardFooter, Card } from "@/components/ui/card"
-import { Label } from "@/components/ui/label"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { PopoverTrigger, PopoverContent, Popover } from "@/components/ui/popover"
-import { Calendar } from "@/components/ui/calendar"
-import { JSX, SVGProps } from "react"
+"use client";
+import {
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Card,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  PopoverTrigger,
+  PopoverContent,
+  Popover,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { JSX, SVGProps } from "react";
+import { Switch } from "./ui/switch";
+import { useStore } from "~/store/useStore";
+import { format } from "date-fns";
 
 export default function TripSelection() {
+  const { from, to, date, time, now, setTo, setDate, setTime, setNow } =
+    useStore();
   return (
-    <Card className="w-full max-w-2xl mx-auto">
+    <Card className="mx-auto w-full max-w-2xl">
       <CardHeader>
         <CardTitle>Trip Selection</CardTitle>
         <CardDescription>Enter your trip details.</CardDescription>
@@ -16,46 +33,58 @@ export default function TripSelection() {
       <CardContent className="space-y-4">
         <div className="space-y-2">
           <Label className="flex justify-start" htmlFor="from">
-          <MapPinIcon className="mr-1 h-4 w-4 -translate-x-1" />
+            <MapPinIcon className="mr-1 h-4 w-4 -translate-x-1" />
             From
-            </Label>
+          </Label>
           <Input id="from" placeholder="Enter departure location" />
         </div>
         <div className="space-y-2">
           <Label className="flex justify-start" htmlFor="to">
-          <MapPinIcon className="mr-1 h-4 w-4 -translate-x-1" />
+            <MapPinIcon className="mr-1 h-4 w-4 -translate-x-1" />
             To
-            </Label>
+          </Label>
           <Input id="to" placeholder="Enter destination location" />
         </div>
         <div className="grid grid-cols-2 gap-4">
           <div className="space-y-2">
-            <Label htmlFor="departure-date">Departure Date</Label>
+            <Label
+              className={now ? "text-[#858484]" : ""}
+              htmlFor="departure-date"
+            >
+              Departure Date
+            </Label>
             <Popover>
-              <PopoverTrigger asChild>
-                <Button className="w-full justify-start text-left font-normal" variant="outline">
+              <PopoverTrigger disabled={now} asChild>
+                <Button
+                  className="w-full justify-start text-left font-normal"
+                  variant="outline"
+                >
                   <CalendarDaysIcon className="mr-1 h-4 w-4 -translate-x-1" />
-                  Select Date
+                  {
+                    date ? (format(date, "PPP")) : (<span>Select Date</span>)
+                  }
                 </Button>
               </PopoverTrigger>
               <PopoverContent align="start" className="w-auto p-0">
-                <Calendar id="departure-date" initialFocus mode="single" />
+                <Calendar
+                  id="departure-date"
+                  mode="single"
+                  selected={date}
+                  onSelect={(date: Date | undefined) => setDate(date ?? new Date())}
+                  initialFocus
+                />
               </PopoverContent>
             </Popover>
           </div>
           <div className="space-y-2">
-            <Label htmlFor="return-date">Return Date</Label>
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button className="w-full justify-start text-left font-normal" variant="outline">
-                  <CalendarDaysIcon className="mr-1 h-4 w-4 -translate-x-1" />
-                  Select Date
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent align="start" className="w-auto p-0">
-                <Calendar id="return-date" initialFocus mode="single" />
-              </PopoverContent>
-            </Popover>
+            <Label htmlFor="now-date">Now: </Label>
+            <div className="flex w-auto items-center">
+              <Switch
+                id="now-date"
+                checked={now}
+                onCheckedChange={() => setNow(!now)}
+              />
+            </div>
           </div>
         </div>
       </CardContent>
@@ -65,10 +94,12 @@ export default function TripSelection() {
         </Button>
       </CardFooter>
     </Card>
-  )
+  );
 }
 
-function CalendarDaysIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
+function CalendarDaysIcon(
+  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>,
+) {
   return (
     <svg
       {...props}
@@ -93,7 +124,7 @@ function CalendarDaysIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElemen
       <path d="M12 18h.01" />
       <path d="M16 18h.01" />
     </svg>
-  )
+  );
 }
 
 function MapPinIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
@@ -113,5 +144,5 @@ function MapPinIcon(props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) {
       <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
       <circle cx="12" cy="10" r="3" />
     </svg>
-  )
+  );
 }
