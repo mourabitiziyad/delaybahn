1cbaa8c073e812ae613673f42b9243bd8d893026
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-12-09 23:18:53 +0100

    ✨ feat: Update tripRouter to use relative import paths and add data validation

--- src/server/api/routers/trip.ts
+++ src/server/api/routers/trip.ts
@@ -1,17 +1,54 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
 
+const LocationSchema = z.object({
+    type: z.string(),
+    id: z.string(),
+    latitude: z.number(),
+    longitude: z.number(),
+});
+
+const ProductsSchema = z.object({
+    nationalExpress: z.boolean(),
+    national: z.boolean(),
+    regionalExpress: z.boolean(),
+    regional: z.boolean(),
+    suburban: z.boolean(),
+    bus: z.boolean(),
+    ferry: z.boolean(),
+    subway: z.boolean(),
+    tram: z.boolean(),
+    taxi: z.boolean(),
+});
+
+const StopSchema = z.object({
+    type: z.string(),
+    id: z.string(),
+    name: z.string(),
+    location: LocationSchema,
+    products: ProductsSchema,
+});
+
+// If the API returns an array of Stops
+const StopsArraySchema = z.array(StopSchema);
+
+// Use StopsArraySchema for validation in your tRPC router
 export const tripRouter = createTRPCRouter({
   getStation: publicProcedure
     .input(z.object({ query: z.string() }))
     .mutation(async ({ input }) => {
       try {
-        const res = await fetch(`https://v6.db.transport.rest/locations?query=${input.query}&fuzzy=true&results=10&stops=true&addresses=false&poi=false&linesOfStops=false&language=en`)
-        const data = await res.json()
-        return data;
+        const res = await fetch(`https://v6.db.transport.rest/locations?query=${input.query}&fuzzy=true&results=20&stops=true&addresses=false&poi=false&linesOfStops=false&language=en`);
+        const data = await res.json();
+        const parsedData = StopsArraySchema.parse(data); // Validate the data shape
+        return parsedData;
       } catch (error) {
-        console.error(error)
-        return error;
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Something went wrong",
+          cause: error,
+        });
       }
     }),
 });
