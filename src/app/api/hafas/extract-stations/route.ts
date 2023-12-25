2f7675271c27f8a6e580ad89456b12dddf5b6c81
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-12-25 18:22:10 +0100

    Refactor departure retrieval and storage

--- src/app/api/hafas/extract-stations/route.ts
+++ src/app/api/hafas/extract-stations/route.ts
@@ -26,101 +26,113 @@ export async function GET() {
     remarks: string;
   }[] = [];
 
-  const departurePromises = stops.map(async (stop) => {
-    const { departures } = await client.departures(stop.id, {
-      linesOfStops: false,
-      remarks: true,
-      products: {
-        nationalExpress: true,
-        national: true,
-        regionalExpress: true,
-        regional: true,
-        suburban: false,
-        bus: false,
-        ferry: true,
-        subway: false,
-        tram: false,
-        taxi: false,
-      },
-      when: undefined, // corresponds to Date.now()
-      duration: 61, // show departures for the next n mins
-      results: 1000, // show departures for the next n mins
-    });
+  try {
 
-    // store trip ids in departures in arrays
-    for (const departure of departures) {
-      if (departure.delay !== null || departure.cancelled) {
-        if (
-          departure.stop?.id &&
-          departure.destination?.id &&
-          departure.line?.id &&
-          departure.plannedWhen
-        ) {
-          departuresList.push({
-            id: departure.tripId,
-            depId: departure.stop?.id,
-            depName: departure.stop?.name || null,
-            depDelay: departure.delay ?? NaN,
-            depDate: new Date(departure.plannedWhen),
-            arrId: departure.destination?.id,
-            arrName: departure.destination?.name,
-            trainId: departure.line?.id,
-            trainType: departure.line?.product,
-            trainName: departure.line?.name,
-            remarks: JSON.stringify(departure.remarks || null),
-            cancelled: departure.cancelled || false,
-          });
+    const departurePromises = stops.map(async (stop) => {
+      const { departures } = await client.departures(stop.id, {
+        linesOfStops: false,
+        remarks: true,
+        products: {
+          nationalExpress: true,
+          national: true,
+          regionalExpress: true,
+          regional: true,
+          suburban: false,
+          bus: false,
+          ferry: true,
+          subway: false,
+          tram: false,
+          taxi: false,
+        },
+        when: undefined, // corresponds to Date.now()
+        duration: 61, // show departures for the next n mins
+        results: 1000, // show departures for the next n mins
+      });
+  
+      // store trip ids in departures in arrays
+      for (const departure of departures) {
+        if (departure.delay !== null || departure.cancelled) {
+          if (
+            departure.stop?.id &&
+            departure.destination?.id &&
+            departure.line?.id &&
+            departure.plannedWhen
+          ) {
+            departuresList.push({
+              id: departure.tripId,
+              depId: departure.stop?.id,
+              depName: departure.stop?.name || null,
+              depDelay: departure.delay ?? NaN,
+              depDate: new Date(departure.plannedWhen),
+              arrId: departure.destination?.id,
+              arrName: departure.destination?.name,
+              trainId: departure.line?.id,
+              trainType: departure.line?.product,
+              trainName: departure.line?.name,
+              remarks: JSON.stringify(departure.remarks || null),
+              cancelled: departure.cancelled || false,
+            });
+          }
         }
       }
-    }
-    console.log("departures for " + stop.name + ": " + departures.length);
-  });
-
-  await Promise.all(departurePromises);
-  if (departuresList.length === 0) {
-    console.log("No departures found");
-    return Response.json(departuresList);
-  } else {
-    const data = await db.trip.createMany({
-      data: departuresList,
-      skipDuplicates: true,
+      console.log("departures for " + stop.name + ": " + departures.length);
     });
-    tripsAddedCount = data.count;
-    console.log(data.count + " departures stored in database with Prisma skipDuplicates");
+  
+    await Promise.all(departurePromises);
+    if (departuresList.length === 0) {
+      console.log("No departures found");
+      return Response.json(departuresList);
+    } else {
+      const data = await db.trip.createMany({
+        data: departuresList,
+        skipDuplicates: true,
+      });
+      tripsAddedCount = data.count;
+      console.log(data.count + " departures stored in database with Prisma skipDuplicates");
+    }
+    console.log("Done");
+  
+    const uniqueDepartures = departuresList.filter(
+      (departure, index, self) =>
+        index ===
+        self.findIndex(
+          (t) =>
+            t.id === departure.id &&
+            t.depId === departure.depId &&
+            t.depName === departure.depName &&
+            t.depDate === departure.depDate &&
+            t.arrId === departure.arrId &&
+            t.arrName === departure.arrName &&
+            t.depDelay === departure.depDelay &&
+            t.trainId === departure.trainId &&
+            t.trainName === departure.trainName &&
+            t.cancelled === departure.cancelled,
+        ),
+    );
+    console.log("unique departures in manual filter: " + uniqueDepartures.length);
+    console.log(
+      "duplicate departures in manual filter: " +
+        (departuresList.length - uniqueDepartures.length),
+    );
+    console.log("total departures: " + departuresList.length);
+    return new Response(JSON.stringify(departuresList.length > 0 ? {success: true, message: `${tripsAddedCount} Added to Database`} : {success: false}), {
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
+    }); 
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({success: false, message: error}), {
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
+    }); 
   }
-  console.log("Done");
 
-  const uniqueDepartures = departuresList.filter(
-    (departure, index, self) =>
-      index ===
-      self.findIndex(
-        (t) =>
-          t.id === departure.id &&
-          t.depId === departure.depId &&
-          t.depName === departure.depName &&
-          t.depDate === departure.depDate &&
-          t.arrId === departure.arrId &&
-          t.arrName === departure.arrName &&
-          t.depDelay === departure.depDelay &&
-          t.trainId === departure.trainId &&
-          t.trainName === departure.trainName &&
-          t.cancelled === departure.cancelled,
-      ),
-  );
-  console.log("unique departures in manual filter: " + uniqueDepartures.length);
-  console.log(
-    "duplicate departures in manual filter: " +
-      (departuresList.length - uniqueDepartures.length),
-  );
-  console.log("total departures: " + departuresList.length);
 
-  return new Response(JSON.stringify(departuresList.length > 0 ? {success: true, message: `${tripsAddedCount} Added to Database`} : {success: false}), {
-    headers: {
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-    },
-  }); 
 }
 
 export const fetchCache = 'force-no-store';
+export const revalidate = 0;
 
 // localhost:3000/api/hafas/extract-stations
