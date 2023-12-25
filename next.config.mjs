06bf97d60358e02f1ca204d5274592371412694f
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2023-12-25 17:14:37 +0100

    Add cache control headers to all routes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,20 @@ const config = {
     images: {
         domains: ["lh3.googleusercontent.com"],
     },
+    async headers() {
+        return [
+          {
+            // Apply these headers to all routes in your application.
+            source: '/:path*',
+            headers: [
+              {
+                key: 'Cache-Control',
+                value: 'no-cache, no-store, must-revalidate',
+              },
+            ],
+          },
+        ];
+      },
 };
 
 export default config;
