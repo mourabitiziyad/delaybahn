0eaade90e95357c22070859dab2ca0b4b7bbc3a9
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-01-15 21:32:02 +0100

    ✨ feat Add JourneyDelaysType and DelayStoreType to useStore.ts

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -49,3 +49,36 @@ export const useJourneyStore = create<JourneyStoreType>((set) => ({
     setJourney: (journey: JourneyResponse) => set(() => ({ journey })),
     reset: () => set(() => ({ journey: undefined })),
 }));
+
+export interface JourneyDelaysType {
+    depId: string;
+    depName: string | null;
+    arrId: string;
+    arrName: string | null;
+    trainType: string;
+    avgDelay: number | null;
+    minDelay: number | null;
+    maxDelay: number | null;
+    numOfTrips: number | null;
+    numOfCancellations: number | null;     
+}
+export interface DelayStoreType {
+    delays: JourneyDelaysType[];
+    setDelays: (delays: JourneyDelaysType[]) => void;
+    reset: () => void;
+}
+
+export interface DelayStoreType {
+    delays: JourneyDelaysType[]; // Update the type to always be an array
+    setDelays: (delays: JourneyDelaysType[]) => void;
+    reset: () => void;
+}
+
+export const useDelayStore = create<DelayStoreType>((set) => ({
+    delays: [],
+    setDelays: (delays) => set((state) => {
+        const uniqueDelays = delays.filter((delay) => !state.delays.some((d) => d.depId === delay.depId && d.arrId === delay.arrId && d.trainType === delay.trainType));
+        return { delays: [...state.delays, ...uniqueDelays] };
+    }),
+    reset: () => set(() => ({ delays: [] })),
+}));
