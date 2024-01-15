ca097648463c984830accc0d640cc62523270fee
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-01-15 23:25:51 +0100

    🚧 temp: Commented out Stats component in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function Home() {
         </h1>
         <div className="flex flex-1 w-full gap-2">
           <TripSelection />
-          <Stats />
+          {/* <Stats /> */}
         </div>
         <JourneyList />
       </div>
